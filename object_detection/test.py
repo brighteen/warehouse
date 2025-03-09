0185cdaafa55dbe68e9055bcfc149dfd90efd1b3
여명구 <88411212+brighteen@users.noreply.github.com>
2025-03-09 21:36:11 +0900

    닭 사진 디텍션

--- object_detection/test.py
+++ object_detection/test.py
@@ -3,7 +3,7 @@ import cv2
 
 model = YOLO("yolov8s.pt") # 원하는 크기 모델 입력(n ~ x)
 
-result = model.predict("./test.jpeg", save=False, conf=0.5)
+result = model.predict("./test4.jpg", save=True, conf=0.5)
 plots = result[0].plot()
 
 cv2.imshow("plot", plots)
